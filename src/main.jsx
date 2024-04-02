087b09dd9624ae37c6e6b20c1d771e2c7738f77e
Martin Aguero <martin.aguero84@gmail.com>
2024-04-02 14:36:15 -0500

    added files structure for my components and pages subfolders within the src folder.

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,6 @@
 import ReactDOM from 'react-dom/client'
+// Brining in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowswerRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
